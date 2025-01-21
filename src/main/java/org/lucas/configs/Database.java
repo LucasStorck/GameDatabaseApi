dff6418f37795fef8783734f03e44ddddd3e0155
Lucas Storck <lucasstorck@icloud.com>
2025-01-21 15:35:41 -0300

    Removal of Username and Password

--- src/main/java/org/lucas/configs/Database.java
+++ src/main/java/org/lucas/configs/Database.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class Database {
 
   private static final String URL = "jdbc:postgresql://localhost:5432/game_db";
-  private static final String USER = "postgres";
-  private static final String PASSWORD = "password";
+  private static final String USER = "#";
+  private static final String PASSWORD = "#";
 
   public static Connection getConnection() throws SQLException{
     return DriverManager.getConnection(URL, USER, PASSWORD);
